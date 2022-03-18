94e8597468e3c070e0ba7623e52a9f607b43c8b2
Juuso <juuso.jahnukainen@gmail.com>
2022-03-18 20:17:44 +0200

    Install script improvements

--- install.sh
+++ install.sh
@@ -9,15 +9,20 @@ fi
 
 if [ -x "$(command -v apt-get)" ]; then
   echo "Installing using apt-get"
-  sudo apt-get install vim zsh git stow
+  sudo apt-get install -y vim zsh git stow
   if [ $1 == "full" ]; then
-    sudo apt-get install xscreensaver rxvt-unicode unclutter firefox compton
+    sudo apt-get install -y xscreensaver rxvt-unicode unclutter firefox compton
   fi
 elif [ -x "$(command -v yum)" ]; then
   echo "Installing using yum"
-  sudo zypper install zsh git vim stow
+  sudo yum install -y zsh git vim stow nano
   if [ $1 == "full" ]; then
-    sudo yum install xscreensaver rxvt-unicode unclutter firefox compton
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    sudo dnf check-update
+    sudo dnf install -y code
+    sudo yum check-update
+    sudo yum install -y xscreensaver rxvt-unicode firefox compton
   fi
 else
   echo "No supported package manager installed!"
